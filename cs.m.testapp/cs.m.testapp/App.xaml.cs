52ad3b554a01e76e34ab587a3d4b19b30ecb6988
Clemens Baumgartner <c.baumgartner@sofa1.at>
2024-03-12 09:58:39 +0100

    Xamarin-TestApp

---
+++ cs.m.testapp/cs.m.testapp/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Xamarin.Forms;
+
+[assembly: ExportFont("Futura-Std-Book-Regular.ttf", Alias = "Futura-Std-Book-Regular")]
+namespace cs.m.testapp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
